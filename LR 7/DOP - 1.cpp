0d3cf51f2cdbda7ffd1b1e645e152e76d4a223a5
WyTrapT <40692433+WyTrapT@users.noreply.github.com>
2018-12-22 15:17:19 +0300

    Add files via upload

---
+++ LR 7/DOP - 1.cpp
@@ -0,0 +1,25 @@
+﻿// DOP - 1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int A = 47;
+	int arr[3] = { 5, 47, 89 };
+
+	for (int i = 0, rez = 0; i < 3; i++, rez = 0)
+	{
+		while (rez < A)
+			rez += arr[i];
+	
+		if (rez == A) cout << "\n делится - " << arr[i];
+		else cout << "\n не делится - " << arr[i];
+	}
+}
