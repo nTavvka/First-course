8fdacd3705de3262c2eb7ec4d804f8662bd0bcdc
WyTrapT <40692433+WyTrapT@users.noreply.github.com>
2019-01-02 09:12:32 +0300

    Add files via upload

---
+++ LR 7/Ex - 3.cpp
@@ -0,0 +1,172 @@
+﻿
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+void summa(double A, double B);
+int* summa_min(double A, double B);
+void prc(int *sum_min);
+void difference(double A, double B);
+void dif(int rez);
+
+int main()
+{
+	//double A = 0.0000001, B = 0.0000101;
+	double A = 1.1111010, B = 0.0000001; /// -6, 1
+	//double A = 1.1100101, B = 0.0010010; /// -27, 18
+	//double A = 1.1111101, B = 1.1111110; /// -3, -2
+	//double A = 0.0000001, B = 1.1111110; /// 1, -2
+	//double A = 1.1111100, B = 0.0000111; /// -4, 7
+	//double A = 1.1110100, B = 0.0001010; /// -12, 10
+
+
+	if (int(A) == 0 && int(B) == 0) summa(A, B);
+	if (int(A) == 1 && int(B) == 1) prc(summa_min(A, B));
+	if (int(A) == 1 && int(B) == 0 || int(A) == 0 && int(B) == 1) difference(A, B);
+
+	return 0;
+}
+
+/// для положительных
+void summa(double A, double B)
+{
+	int sum = (A + B) * 10000000;
+
+	int rezult[8];
+	for (int i = 7; i > -1; i--)
+	{
+		rezult[i] = sum % 10;
+		sum /= 10;
+	}
+
+	for (int i = 7; i > -1; i--)
+	{
+		while (rezult[i] > 1)
+		{
+			rezult[i] -= 2;
+			rezult[i - 1] += 1;
+		}
+	}
+
+	for (int i = 0; i < 8; i++)
+		cout << rezult[i];
+}
+/// для отрицательных
+int* summa_min(double A, double B)
+{
+	int rezult = (A + B) * 10000000;
+	//cout << rezult << endl;
+	//if (rezult % 10 == 9) rezult -= 9;
+
+	int *rez_arr = new int[8];
+	for (int i = 7; i > -1; i--)
+	{
+		rez_arr[i] = rezult % 10;
+		rezult /= 10;
+	}
+
+	for (int i = 7; i > -1; i--)
+	{
+		while (rez_arr[i] > 1)
+		{
+			rez_arr[i] -= 2;
+			if (i - 1 == 0) break;
+			rez_arr[i - 1] += 1;
+		}
+	}
+	rez_arr[0] = 1;
+
+	return rez_arr;
+}
+void prc(int *sum_min)
+{
+	int a = 0;
+
+	for (int i = 1; i < 8; i++)
+	{
+		a = a * 10 + sum_min[i];
+	}
+	//cout << "\n a = " << a;
+	a -= 1;
+	//cout << "\n a = " << a;
+
+	int *rezult = new int[8];
+	for (int i = 7; i > -1; i--)
+	{
+		rezult[i] = a % 10;
+		a /= 10;
+	}
+
+	for (int i = 0; i < 8; i++)
+	{
+		if (rezult[i] >= 1) rezult[i] = 0;
+		else rezult[i] = 1;
+	}
+	rezult[0] = 1;
+
+	//cout << "\n ----\n";
+	for (int i = 0; i < 8; i++)
+	{
+		cout << rezult[i];
+	}
+	delete[] rezult;
+}
+/// положительный и отрицательный
+void difference(double A, double B)
+{
+	int diff = (A + B) * 10000000;
+	diff -= 1;
+	//cout << diff << endl;
+	int *rez = new int[8];
+	for (int i = 7; i > -1; i--)
+	{
+		rez[i] = diff % 10;
+		diff /= 10;
+	}
+
+	for (int i = 7; i > -1; i--)
+	{
+		while (rez[i] > 1)
+		{
+			if (rez[i] == 9) break;
+			rez[i] -= 2;
+			rez[i - 1] += 1;
+		}
+	}
+	if (rez[0] == 0) dif((A+B) * 10000000);
+	else
+	{
+		for (int i = 1; i < 8; i++)
+		{
+			if (rez[i] >= 1) rez[i] = 0;
+			else if (rez[i] == 0) rez[i] = 1;
+		}
+
+		for (int i = 0; i < 8; i++)
+			cout << rez[i];
+	}
+}
+void dif(int rez)
+{
+	//cout << rez;
+
+	int *dif = new int[8];
+	for (int i = 7; i > -1; i--)
+	{
+		dif[i] = rez % 10;
+		rez /= 10;
+	}
+
+	for (int i = 7; i > -1; i--)
+	{
+		while (dif[i] > 1)
+		{
+			dif[i] -= 2;
+			dif[i - 1] += 1;
+		}
+	}
+
+	for (int i = 0; i < 8; i++)
+		cout << dif[i];
+}
---
+++ LR 7/Ex - 4.cpp
@@ -0,0 +1,213 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void solve_sum(string A, string B);
+int* add_one(int *rezult, int &rez_size);
+void solve_diff(string A, string B);
+string point(string A, int counter);
+int cmp(string A, string B);
+
+int main()
+{
+	//string signA = "+", signB = "+";
+	//string A = "1D4A", B = "11E";
+
+	string signA = "+", signB = "+";
+	string A = "34", B = "26";
+
+	/*string signA = "+", signB = "+";
+	string A = "BD", B = "BD";*/
+
+	if (signA == signB) solve_sum(A, B);
+
+	if (signA == "+" && signB == "-")
+	{
+		if (cmp(A, B) == 0 || cmp(A, B) == 2) solve_diff(A, B);
+		else if (cmp(A, B) == 1)
+		{
+			cout << "-";
+			solve_diff(B, A);
+		}
+	}
+	else if (signA != signB && cmp(A, B) == 2) return 0;
+	else if (signA == "-" && signB == "+")
+	{
+		if (cmp(A, B) == 1 || cmp(A, B) == 2) solve_diff(B, A);
+		else if (cmp(A, B) == 0)
+		{
+			cout << "-";
+			solve_diff(A, B);
+		}
+	}
+	return 0;
+}
+
+void solve_sum(string A, string B)
+{
+	reverse(A.begin(), A.end());
+	reverse(B.begin(), B.end());
+
+	int size = 0;
+
+	int rez_size = 1;
+	int *rezult = new int[rez_size];
+	rezult[0] = 0;
+
+	if (A.size() >= B.size()) size = A.size();
+	else size = B.size();
+
+	int counter = 0;
+	for (int i = 0, a = 0; i < size; i++, a = 0, counter = 0)
+	{
+		/// буква с буквой
+		if (A[i] >= 'A' && B[i] >= 'A')
+			a = A[i] + B[i] - 'A' - 'A' + 20;
+		/// цифра с цифрой 
+		if (A[i] < 58 && B[i] < 58)
+			a = A[i] + B[i] - '0' - '0';
+		/// буква с цифрой
+		if (A[i] >= 'A' && B[i] < 58 || A[i] < 58 && B[i] >= 'A')
+			a = A[i] + B[i] - 'A' - '0' + 10;
+		/// буква и 0
+		if (A[i] >= 'A' && B[i] == 0)
+			a = A[i] - 'A' + 10;
+		else if (B[i] >= 'A' && A[i] == 0)
+			a = B[i] - 'A' + 10;
+		/// цифра и 0
+		if (A[i] < 58 && B[i] == 0)
+			a = A[i] - '0';
+		else if (B[i] < 58 && A[i] == 0)
+			a = B[i] - '0';
+
+		rezult[rez_size - 1] += a;
+		while (true)
+		{
+			if (rezult[rez_size - 1] > 14)
+			{
+				counter++;
+				rezult[rez_size - 1] -= 15;
+				rezult = add_one(rezult, rez_size);
+				rezult[rez_size - 1] += 1;
+			}
+			else break;
+		}
+		if (counter == 0 && (i + 1) < size) rezult = add_one(rezult, rez_size);
+	}
+
+	string sum = "";
+	for (int i = rez_size - 1; i > -1; i--)
+	{
+		if (rezult[i] < 10) sum += rezult[i] + '0';
+		else sum += rezult[i] + 'A' - 10;
+	}
+	cout << sum;
+	delete[] rezult;
+}
+int* add_one(int *rezult, int &rez_size)
+{
+	rez_size++;
+	int *temp = new int[rez_size];
+
+	for (int i = 0; i < rez_size - 1; i++)
+		temp[i] = rezult[i];
+	delete[] rezult;
+
+	rezult = temp;
+	rezult[rez_size - 1] = 0;
+
+	return rezult;
+}
+void solve_diff(string A, string B)
+{
+	reverse(A.begin(), A.end());
+	reverse(B.begin(), B.end());
+
+	int size = 0;
+
+	int rez_size = 1;
+	int *rezult = new int[rez_size];
+	rezult[0] = 0;
+
+	if (A.size() >= B.size()) size = A.size();
+	else size = B.size();
+
+	int a = 0;
+	for (int i = 0; i < size; i++, a = 0)
+	{
+		/// буква и буква
+		if (A[i] >= 'A' && B[i] >= 'A')
+		{
+			if (A[i] < B[i]) A = point(A, i);
+			a = A[i] - B[i];
+			//else if (A[i] < B[i])
+
+		}
+		/// буква и 0
+		else if (A[i] >= 'A' && B[i] == 0)
+			a = A[i] - 'A' + 10;
+		/// буква и цифра
+		else if (A[i] >= 'A' && B[i] < 58 && B[i] >= '0')
+			a = A[i] - 'A' + 10 - B[i] + '0';
+		/// цифра и цифра
+		else if (A[i] < 58 && B[i] < 58 && B[i] >= '0')
+		{
+			if (A[i] < B[i]) A = point(A, i);
+			a = A[i] - B[i];
+		}
+		/// цифра и 0
+		else if (A[i] < 58 && B[i] == 0)
+		{
+			a = A[i] - '0';
+		}
+
+		rezult[rez_size - 1] = a;
+		//cout << A << endl;
+		if (i + 1 < size) rezult = add_one(rezult, rez_size);
+	}
+
+	string diff = "";
+	for (int i = rez_size - 1; i > -1; i--)
+	{
+		if (rezult[i] < 10) diff += rezult[i] + '0';
+		else diff += rezult[i] + 'A' - 10;
+	}
+	//reverse(diff.begin(), diff.end());
+	cout << diff;
+	delete[] rezult;
+}
+string point(string A, int counter)
+{
+	if (A[counter + 1] != 'A')
+	{
+		A[counter + 1] -= 1;
+		A[counter] += 15;
+	}
+	else if (A[counter + 1] == 'A')
+	{
+		A[counter + 1] = '9';
+		if (A[counter] == 9) A[counter] = 'A';
+		else A[counter] += 15;
+	}
+	return A;
+}
+int cmp(string A, string B)
+{
+	int counterA = 0;
+	for (int i = 0; i < A.size(); i++)
+		if (A[i] < 58) counterA += (A[i] - '0') * pow(10, A.size() - 1 - i);
+		else counterA += (A[i] - 'A' + 10) * pow(10, A.size() - 1 - i);
+
+	int counterB = 0;
+	for (int i = 0; i < B.size(); i++)
+		if (B[i] < 58) counterB += (B[i] - '0') * pow(10, B.size() - 1 - i);
+		else counterB += (B[i] - 'A' + 10) * pow(10, B.size() - 1 - i);
+
+	//cout << "\n A = " << counterA << "\n B = " << counterB << endl;
+	if (counterA > counterB) return 0;
+	if (counterA < counterB) return 1;
+	if (counterA == counterB) return 2;
+}
